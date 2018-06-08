0e5178a978b74528c727dd4882fd4b8782256fc7
kanna.hirao <kanna.hirao@rakuten.com>
2018-06-08 07:39:21 +0000

    fix2

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
-Route::get('login', 'Auth\LoginController@logout')->name('logout.get');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
