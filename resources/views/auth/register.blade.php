492e81e7c0b2b2f0bc5ade7f9d2b2d942a90e9f5
kanna.hirao <kanna.hirao@rakuten.com>
2018-06-08 07:21:17 +0000

    fix1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
             
-            {!! From::open(['route' => 'signup.post']) !!}
+            {!! Form::open(['route' => 'signup.post']) !!}
                 <div class="form-group">
                     {!! Form::label('name', 'Name') !!}
                     {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
@@ -16,17 +16,17 @@
                 
                 <div class="form-group">
                     {!! Form::label('email', 'Email') !!}
-                    {!! Form::text('email', old('email'), ['class' => 'form-control']) !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                 </div>
                 
                 <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
-                    {!! Form::text('password', old('password'), ['class' => 'form-control']) !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
                 
                 <div class="form-group">
                     {!! Form::label('password_confirmation', 'Confirmation') !!}
-                    {!! Form::text('password_confirmation', ['class' => 'form-control']) !}}
+                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                 </div>
                 
                 {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
