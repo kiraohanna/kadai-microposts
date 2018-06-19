05d0a5b3db1a2efbcc211519323502bf09d00261
kanna.hirao <kanna.hirao@rakuten.com>
2018-06-19 04:13:03 +0000

    favoritebutton fixed

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -12,12 +12,14 @@
             <div>
                 <p>{!! nl2br(e($micropost->content)) !!}</p>
             </div>
-            <div>
+            <div class="btn-group">
                 @if (Auth::user()->id == $micropost->user_id)
                     {!! Form::open(['route'=>['microposts.destroy', $micropost->id], 'method'=>'delete']) !!}
                         {!! Form::submit('Delete', ['class'=>'btn btn-danger btn-xs']) !!}
                     {!! Form::close() !!}
                 @endif
+            </div>
+            <div class="btn-group">
                 @include('favorites.favorite_button')
             </div>
         </div>
